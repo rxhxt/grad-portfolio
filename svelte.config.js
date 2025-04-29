4a2ebb8e4dc479682f9e6124ef164a3095b03ede
rxhxt <rohit37.pict@gmail.com>
2025-04-28 21:14:28 -0700

    fixed url

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/grad-portfolio/';
+const base = '/grad-portfolio';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
